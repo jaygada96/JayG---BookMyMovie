874803b9a77f3688c8cab49fe438f9fcb2db5e08
jaygada96 <jay.gada96@gmail.com>
2022-08-21 19:00:12 +0530

    Fixed Home.css for minor errors

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,9 +1,7 @@
 import React from "react";
-import Home from "../screens/home/Home";
-//import Details from "../screens/details/Details";
-import Details from "./details/Details";
-//import { Details } from "@material-ui/icons";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "../screens/home/Home";
+import Details from "./details/Details";
 import BookShow from "../screens/bookshow/BookShow";
 import Confirmation from "../screens/confirmation/Confirmation";
 
